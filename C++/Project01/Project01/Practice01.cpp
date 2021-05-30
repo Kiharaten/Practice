314088995b2c6634c5f42c9a444bd49466830b30
Sora Kihara <sorakihara@SoranoiMac.local>
2021-05-30 14:25:37 +0900

    add Practice01 that how to use VisualStudio & cpp

---
+++ C++/Project01/Project01/Practice01.cpp
@@ -0,0 +1,30 @@
+// #include <bits/stdc++.h> とりあえず使わない
+#include <iostream>
+
+using namespace std;
+// 最初に書く:これがあると宣言を省略できる
+
+int main() {
+	cout << "Hello, world!" << endl; //Hello, worldと表示
+	/*ここも
+	コメント*/
+
+	cout << "a";
+	cout << "b" << endl;
+	cout << "c" << "d" << endl;
+	cout << "文字列出力" << endl;
+
+	cout << 2525 << endl;
+	cout << "数字出力" << endl;
+}
+
+
+/***** 基本形 *****
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+}
+
+***** 基本形 *****/
