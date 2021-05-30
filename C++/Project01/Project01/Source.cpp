aa00fbd2ec8913f63a2badc617e7a7bd35f507c4
Sora Kihara <sorakihara@SoranoiMac.local>
2021-05-30 14:13:58 +0900

    add a Source.cpp

---
+++ C++/Project01/Project01/Source.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h> とりあえず使わない
+#include <iostream>
+
+using namespace std;
+// 最初に書く
+
+int main() {
+	cout << "Hello, world!" << endl;
+}
+
+
+/***** 基本形 *****
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+}
+
+***** 基本形 *****/
