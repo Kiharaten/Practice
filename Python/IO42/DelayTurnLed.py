2bfb2d8157cce6fc22ede15d7c73df44e5958fde
Kiharaten <kiharaten1129@gmail.com>
2021-06-30 15:30:58 +0900

    file edit

--- Python/IO42/DelayTurnLed.py
+++ Python/IO42/DelayTurnLed.py
@@ -1,7 +1,39 @@
 # coding:utf-8
-
 import RPi.GPIO as GPIO
 import time
+
+def countSec(second,  type):                                                                             
+    """                                                                                                  
+    指定した秒数を数える                                                                                 
+    正常終了したかどうかでTrue or Falseを返す                                                            
+                                                                                                         
+    第一引数:秒数(sec), 第二引数:判定("ON" or "OFF")                                                     
+    """                                                                                                  
+    cnt = 0                                                                                              
+    timer = second * 10                                                                                  
+
+    if type == "ON":                                                                                     
+        while GPIO.input(SW) and cnt < timer: # 0.1秒刻みで押している時間を数える                        
+            time.sleep(0.1)                                                                              
+            cnt = cnt + 1                                                                                
+        else:                                                                                            
+            if timer <= cnt: # 正常終了の場合True, 異常終了の場合Falseを返す                             
+                return True                                                                              
+            else:                                                                                        
+                return False                                                                             
+
+    elif type == "OFF":                                                                                  
+        while not GPIO.input(SW) and cnt < timer: # 0.1秒刻みで離している時間を数える                    
+            time.sleep(0.1)                                                                              
+            cnt = cnt + 1                                                                                
+        else:                                                                                            
+            if timer < cnt: # 正常終了の場合False, 異常終了の場合TTrueを返す                             
+                return False                                                                             
+            else:                                                                                        
+                return True                                                                              
+
+    else:                                                                                                
+        return "tyle error"
 
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM)
