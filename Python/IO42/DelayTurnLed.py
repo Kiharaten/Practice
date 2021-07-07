9755e933c5707e38cd647f061bf7d033af1b2115
Kiharaten <kiharaten1129@gmail.com>
2021-07-07 11:42:40 +0900

    ソース削減

--- Python/IO42/DelayTurnLed.py
+++ Python/IO42/DelayTurnLed.py
@@ -2,47 +2,31 @@
 import RPi.GPIO as GPIO
 import time
 
-def countSec(second,  type):
+def countSec(second, type):
     """
-    指定した秒数を数える
+    指定した秒数を0.1秒刻みで数える
     正常終了したかどうかでTrue or Falseを返す
    
-    第一引数:秒数(sec), 第二引数:判定("ON" or "OFF")
+    第一引数:秒数(sec), 第二引数:判定(1 or 0)
     """
     cnt = 0
     timer = second * 10
 
-    if type == "ON":
-        print("\ntype={0}, count timer={1} start".format(type, timer))
-        while GPIO.input(SW) and cnt < timer: # 0.1秒刻みで押している時間を数える
-            time.sleep(0.1)
-            cnt = cnt + 1
+    print("\ntype={0}, count timer={1} start".format(type, timer))
+    while GPIO.input(SW) == type and cnt < timer: # 0.1秒刻みで押している時間を数える
+        print("gpio true ={0}".format(GPIO.input(SW)))
+        time.sleep(0.1)
+        cnt = cnt + 1
+    else:
+        print("count stop")
+        if timer <= cnt: # 正常終了の場合True, 異常終了の場合Falseを返す
+            print("return True")
+            return True
         else:
-            print("count stop")
-            if timer <= cnt: # 正常終了の場合True, 異常終了の場合Falseを返す
-                print("return True")
-                return True
-            else:
-                print("return False")
-                return False
+            print("return False")
+            return False
 
-    elif type == "OFF":
-        print("\ntype={0}, count timer={1} start".format(type,timer))
-        while not GPIO.input(SW) and cnt < timer: # 0.1秒刻みで離している時間を数える
-            time.sleep(0.1)
-            cnt = cnt + 1
-        else:       
-            print("count stop")
-            if timer <= cnt: # 正常終了の場合False, 異常終了の場合TTrueを返す            
-                print("return False")
-                return False         
-            else:   
-                print("return True")
-                return True          
-
-    else:           
-        return "type error"
-
+# ---------- 主処理 ----------
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM)
 
@@ -52,22 +36,17 @@ LED = 17
 GPIO.setup(SW, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(LED, GPIO.OUT, initial = False)
 
-#ループ
 while True:
     if GPIO.input(SW):
-        flg = countSec(3, "ON")
+        flg = countSec(3, 1) # 3秒間GPIO.inputに1が入り続けるとTrueを返す
         if flg:
-            GPIO.output(LED, flg)
+            GPIO.output(LED, True)
             print("switch-ON")
-        else:
-            print("timeout")
 
     elif not GPIO.input(SW):
-        flg = countSec(2, "OFF")
-        if not flg:
-            GPIO.output(LED, flg)
+        flg = countSec(2, 0) # 2秒間GPIO.inputに0が入り続けるとTrueを返す
+        if flg:
+            GPIO.output(LED, False)
             print("switch-OFF")
-        else:
-            print("timeout")
             
 GPIO.cleanup()
