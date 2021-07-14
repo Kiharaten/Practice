1626d5bfe74a8f0386a253eb90638d9a11ee43a2
Kiharaten <kiharaten1129@gmail.com>
2021-07-14 15:37:45 +0900

    fix AlterandMoment

--- Python/IO42/AlterandMoment.py
+++ Python/IO42/AlterandMoment.py
@@ -2,22 +2,22 @@
 import RPi.GPIO as GPIO
 import time
 
-def countSec(timer, type):
+def countSec(type, timer):
     """
     指定した秒数を0.1秒刻みで数える
     正常終了したかどうかでTrue or Falseを返す
    
-    第一引数:秒数(sec * 10), 第二引数:判定(1 or 0)
+    第一引数:判定(1 or 0), 第二引数:秒数(sec * 10)
     """
     cnt = 0
 
-    print("\ntype={0}, count timer={1} start".format(type, timer))
-    while GPIO.input(SW) == type and cnt <= timer: # 0.1秒刻みで押している時間を数える
-        print("gpio true ={0}".format(GPIO.input(SW)))
-        time.sleep(0.1)
+    print("\r" + "\ntype = {0}, timer = {1} count start".format(type, timer))
+    while GPIO.input(SW) == type and cnt < timer: # 0.1秒刻みで押している時間を数える
+        print("gpio = {0}".format(GPIO.input(SW)))
         cnt = cnt + 1
+        time.sleep(0.1)
     else:
-        print("count stop")
+        print("count stop, ", end = "")
         if timer <= cnt: # 正常終了の場合True, 異常終了の場合Falseを返す
             print("return True")
             return True
@@ -43,7 +43,7 @@ GPIO.setup(LED2, GPIO.OUT, initial = False)
 
 while True:
     if GPIO.input(SW):
-        flg = countSec(5, 1)
+        flg = countSec(1, 5)
         if flg:
             GPIO.output(LED1, True)
             GPIO.output(LED2, True)
@@ -51,7 +51,7 @@ while True:
             pushFlg1 = 1
             
     elif not GPIO.input(SW):
-        flg = countSec(5, 0)
+        flg = countSec(0, 5)
         if flg:
             GPIO.output(LED2, False)
             print("switch-OFF")
@@ -63,9 +63,7 @@ while True:
         if cnt % 2 == 0:
             GPIO.output(LED1, False)
     
-    print("pushFlg1 = {0}".format(pushFlg1))
-    print("pushFlg2 = {0}".format(pushFlg2))
-    print("cnt = {0}".format(cnt))
+    print("pushFlg1 = {0}, pushFlg2 = {1}, cnt = {2}".format(pushFlg1, pushFlg2, cnt))
     pushFlg2 = pushFlg1
     
 GPIO.cleanup()
