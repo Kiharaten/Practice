c1a7ce5739583a275175530f22628791ce429423
Kiharaten <kiharaten1129@gmail.com>
2021-07-15 04:42:45 +0900

    fix python file

--- Python/IO42/AlterandMoment.py
+++ Python/IO42/AlterandMoment.py
@@ -2,30 +2,32 @@
 import RPi.GPIO as GPIO
 import time
 import os
+import sys
 
-def countSec(type, timer):
+def count_second(type, timer):
     """
-    指定した秒数を0.1秒刻みで数える
+    指定した秒数を1/10秒刻みで数える
     正常終了したかどうかでTrue or Falseを返す
    
     第一引数:判定(1 or 0), 第二引数:秒数(sec * 10)
     """
     cnt = 0
-
-    print("\ntype = {0}, timer = {1} count start".format(type, timer))
+    print("type = {0}, timer = {1}, count start >>> ".format(type, timer), end = "")  
+    print("GPIO INPUT = ", end = "")
     while GPIO.input(SW) == type and cnt < timer: # 0.1秒刻みで押している時間を数える
-        print("gpio = {0}".format(GPIO.input(SW), end = ""))
+        print(GPIO.input(SW), end = "")
         cnt = cnt + 1
         time.sleep(0.1)
     else:
-        print("count stop, ", end = "")
+        os.system("clear") #GPIOを 数え終わった時点で画面クリア
+        print("\ncount stop, ", end = "")
         if timer <= cnt: # 正常終了の場合True, 異常終了の場合Falseを返す
-            print("return True, ", end = "")
+            print("return True")
             return True
         else:
             print("return False")
             return False
-
+        
 # ---------- 前処理 ----------
 GPIO.setwarnings(False) 
 GPIO.setmode(GPIO.BCM)
@@ -35,7 +37,9 @@ LED1 = 17
 LED2 = 27
 pushFlg1 = 0
 pushFlg2 = 0
-cnt = 0
+pushCnt = 0
+releaseCnt = 0
+text = ""
 
 GPIO.setup(SW, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 GPIO.setup(LED1, GPIO.OUT, initial = False)
@@ -44,24 +48,35 @@ GPIO.setup(LED2, GPIO.OUT, initial = False)
 # ---------- 主処理 ----------
 while True:
     if GPIO.input(SW):
-        if countSec(1, 5):
+        if count_second(1, 5):
             GPIO.output(LED1, True)
             GPIO.output(LED2, True)
-            print("switch-ON")
+            text = "LED1-ON, LED2-ON"
             pushFlg1 = 1
-            
+            if pushFlg1 != pushFlg2:
+                pushCnt = pushCnt + 1
+
     elif not GPIO.input(SW):
-        if countSec(0, 5):
-            GPIO.output(LED2, False)
-            print("switch-OFF")
+        if count_second(0, 5):
+            GPIO.output(LED1, False)
+            text = "LED1-OFF"
             pushFlg1 = 0
             if pushFlg1 != pushFlg2:
-                cnt = cnt + 1
-            if cnt % 2 == 0:
-                GPIO.output(LED1, False)
+                releaseCnt = releaseCnt + 1
+
+            # ---------- オルタネート用
+            if releaseCnt % 2 == 0:
+                GPIO.output(LED2, False)
+                text = text + ", LED2-OFF"
+            else:
+                text = text + ", LED2-ON"
+
+    print(text)
     
-    print("pushFlg1 = {0}, pushFlg2 = {1}, cnt = {2}".format(pushFlg1, pushFlg2, cnt))
+    #print("push flg 1 = {0}, push flg 2 = {1}".format(pushFlg1, pushFlg2))
+    print("push count = {0}, release count = {1}".format(pushCnt, releaseCnt))
     pushFlg2 = pushFlg1
 
-# ---------- 後処理 ----------
+    # ---------- 後処理 ----------
 GPIO.cleanup()
+nnn
