34f01bfa34227d211fca7ffa31f8c7d2fbce596f
Kiharaten <sukerock324@gmail.com>
2021-07-15 10:58:38 +0900

    add python file

---
+++ Python/hoge.py
@@ -0,0 +1,22 @@
+# import sys, time
+# str1 = "print start with GPIO = "
+# for num, i in enumerate(range(10)):
+#     sys.stdout.write("\r" + str1 + "%d" % num)
+#     sys.stdout.flush()
+#     time.sleep(0.1)
+
+# for i in range(10):
+#     sys.stdout.write("=")
+#     sys.stdout.flush()
+#     time.sleep(0.1)
+
+import os
+
+print('Messsage1')
+print('Messsage2')
+print('Messsage3')
+print('Messsage4')
+print('Messsage5')
+input()
+os.system('clear')
+print('End')
