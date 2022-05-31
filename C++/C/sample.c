9cdb9b2c5c6731fbffe9be2b7079345d006d07b7
Kiharaten <sorakihara@KiharaXiMac-2.local>
2022-06-01 00:04:10 +0900

    practice

--- C++/C/sample.c
+++ C++/C/sample.c
@@ -11,15 +11,17 @@ void sample1(int array[2][10]) {
 // sample1:固定長配列のポインタ渡し2
 void sample2(int array[2][10]) {
     int i;
+    int num = 1;
 
     for(i = 0; i < 10; i++){
-        array[1][i] = 1;
+        array[1][i] = num;
+        num *= 2;
     }
 }
 
 // preview:固定長配列のポインタ受け取り
 int main() {
-    // 受け取り用配列の準備
+    // 受け取り配列の準備
     int array[2][10];
     int i;
     int j;
@@ -28,7 +30,7 @@ int main() {
     sample1(array);
     sample2(array);
 
-        // 配列の中身の表示
+    // 配列の中身の表示
     for(i = 0; i < 2; i++) {
         printf("\n// ----- sample%d\n", i + 1);
         for(j = 0; j < 10; j++) {
